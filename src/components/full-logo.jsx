812a26e187b12f14147b32fe5c9d434c0cd92b1d
Vinoth <vnoitkumar@gmail.com>
2024-08-24 15:11:08 +0530

    add alt

--- src/components/full-logo.jsx
+++ src/components/full-logo.jsx
@@ -6,10 +6,12 @@ export function FullLogo() {
       <img
         src="/vnoit-full-dark-color-logo.svg"
         className="h-12 m-auto block dark:hidden"
+        alt="Vnoit dark logo"
       />
       <img
         src="/vnoit-full-light-color-logo.svg"
         className="h-12 m-auto hidden dark:block"
+        alt="Vnoit light logo"
       />
     </>
   );
