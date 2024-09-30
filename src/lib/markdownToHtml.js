1fe4da95b1a7bc9317df080bd0c4b161a16d89ce
Vinoth (Vnoit) <vnoitkumar@gmail.com>
2024-09-30 08:18:13 +0530

    add highlight.js to highlight code

--- src/lib/markdownToHtml.js
+++ src/lib/markdownToHtml.js
@@ -1,7 +1,26 @@
-import { remark } from 'remark';
-import html from 'remark-html';
+import hljs from "highlight.js";
+import Markdown from "markdown-it";
+import "highlight.js/styles/atom-one-dark.css";
+
+const md = Markdown({
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      const options = { language: lang, ignoreIllegals: true };
+      const highlightedCode = hljs.highlight(str, options).value;
+      return `<pre class="hljs"><code>${highlightedCode}</code></pre>`;
+    }
+
+    const code = md.utils.escapeHtml(str);
+    return `<pre class="hljs"><code>${code}</code></pre>`;
+  },
+});
 
 export default async function markdownToHtml(markdown) {
-  const result = await remark().use(html).process(markdown);
-  return result.toString().replaceAll('<a href=', '<a target="_blank" rel="noopener noreferrer nofollow" href=');
+  const result = await md.render(markdown);
+  return result
+    .toString()
+    .replaceAll(
+      "<a href=",
+      '<a target="_blank" rel="noopener noreferrer nofollow" href='
+    );
 }
